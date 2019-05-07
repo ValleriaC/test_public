0cc57caf0ae5a7614840bea79089ce631f584c6f
ValleriaC <42744064+ValleriaC@users.noreply.github.com>
2019-05-07 16:19:38 +0300

    Add files via upload

--- test.php
+++ test.php
@@ -4,6 +4,7 @@ $username = "username";
 $password = "";
 $dbname = "test_jenkins";
 
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -11,19 +12,18 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
+$sql = "SELECT user_id, nume, prenume FROM users";
+$result = $conn->query($sql);
 
- $nume = $_POST['name'];
-    $prenume = $_POST['lname'];
-   
-
-$sql = "INSERT INTO users (nume, prenume)
-VALUES ($nume, $prenume)";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>Name</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["user_id"]."</td><td>".$row["nume"]." ".$row["prenume"]."</td></tr>";
+    }
+    echo "</table>";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "0 results";
 }
-
 $conn->close();
 ?>
